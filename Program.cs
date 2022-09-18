b8aa01424bb9d6eca6664bfb492836970330a607
sworc <lightbox@nifty.com>
2022-09-18 10:09:10 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace cs_0915_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ****************************
+            // 文字列を整数に変換
+            // ****************************
+            var su = "123";
+            var result = int.Parse(su);
+            result++;
+            result += 2;
+            result = result + 2;
+            print(result);
+
+            // ****************************
+            // 整数を文字列に変換
+            // ****************************
+            var text = string.Format("{0}", result);
+            print(text);
+
+            text = $"{result}";
+            print(text);
+
+            // 簡易的な方法
+            text = result + "";
+            print(text);
+
+        }
+
+        private static void print(int v)
+        {
+            System.Console.WriteLine(v);
+            Debug.WriteLine(v);
+        }
+
+        private static void print(string v)
+        {
+            System.Console.WriteLine(v);
+            Debug.WriteLine(v);
+        }
+    }
+}
